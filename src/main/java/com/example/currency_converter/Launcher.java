fbba6871c1a0d19e46c37482d2abc8ab0bebeb22
Shokanta Roy <shokantaroy.2002@gmail.com>
2025-08-19 16:49:55 +0600

    Initial commit - Currency Converter project

---
+++ src/main/java/com/example/currency_converter/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.currency_converter;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
